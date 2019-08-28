0949aa4efd93088aa507fecdc5adeaf6975f129a
lclMetal <lehtinen.lassi.c@gmail.com>
2019-08-28 10:20:29 +0300

    Improve project stability by combining geui.c and layout.c

--- source/geui.c
+++ source/geui.c
@@ -956,8 +956,8 @@ void buildButton(WindowItem *ptr)
         Actor *a;
         a = CreateActor("a_gui", ptr->parent->style.guiAnim, ptr->parent->parentCName, "(none)", 0, 0, true);
         // TODO: actual positioning
-        a->x = -tileWidth + i * tileWidth + (i >= 2 && i >= tilesHorizontal - 2) * (buttonWidth - tilesHorizontal * tileWidth);
-        a->y = -tileHeight + 25 * ptr->index;
+        a->x = tileWidth + i * tileWidth + (i >= 2 && i >= tilesHorizontal - 2) * (buttonWidth - tilesHorizontal * tileWidth);
+        a->y = tileHeight + 25 * ptr->index;
         a->myWindow = ptr->parent->index;
         a->myIndex  = ptr->index;
         ChangeZDepth(a->clonename, 0.35); // TODO: change back to 0.3 after testing
@@ -1186,4 +1186,108 @@ void destroyPanel(Panel *panel)
 
     panel->iList = NULL;
     panel->iIndex = 0;
+}
+
+void updatePanelLayout(Panel *panel)
+{
+    WindowItem *item;
+
+    if (!panel) return;
+
+    for (item = panel->iList; item != NULL; item = item->next)
+    {
+        if (item->layout.row > panel->layout.row)
+            panel->layout.row = item->layout.row + 1;
+        if (item->layout.col > panel->layout.col)
+            panel->layout.col = item->layout.col + 1;
+    }
+
+    panel->layout.width = getPanelWidth(panel);
+    panel->layout.height = getPanelHeight(panel);
+}
+
+void setPosition(WindowItem *this, short row, short col)
+{
+    if (!this) return;
+
+    this->layout.row = row;
+    this->layout.col = col;
+
+    updatePanelLayout(this->myPanel);
+}
+
+short getColWidth(Panel *panel, short col)
+{
+    short width = 0;
+    WindowItem *item;
+
+    if (!panel || !panel->layout.col) return -1;
+    if (col < 0 || col >= panel->layout.col) return -2;
+
+    for (item = panel->iList; item != NULL; item = item->next)
+    {
+        if (item->type == GEUI_Panel) updatePanelLayout(item->data.panel.panel);
+        if (item->layout.col == col && item->layout.width > width)
+            width = item->layout.width;
+    }
+
+    return width;
+}
+
+short getRowHeight(Panel *panel, short row)
+{
+    short height = 0;
+    WindowItem *item;
+
+    if (!panel || !panel->layout.row) return -1;
+    if (row < 0 || row >= panel->layout.row) return -2;
+
+    for (item = panel->iList; item != NULL; item = item->next)
+    {
+        if (item->type == GEUI_Panel) updatePanelLayout(item->data.panel.panel);
+        if (item->layout.row == row && item->layout.height > height)
+            height = item->layout.height;
+    }
+
+    return height;
+}
+
+short getPanelWidth(Panel *panel)
+{
+    short col;
+    short width = 0;
+    short tempWidth;
+
+    if (!panel) return -1;
+    updatePanelLayout(panel);
+
+    if (!panel->layout.col) return -2;
+
+    for (col = 0; col < panel->layout.col; col ++)
+    {
+        if ((tempWidth = getColWidth(panel, col)) >= 0)
+            width += tempWidth;
+    }
+
+    return width;
+}
+
+short getPanelHeight(Panel *panel)
+{
+    short row;
+    short height = 0;
+    short tempHeight;
+
+    if (!panel) return -1;
+    updatePanelLayout(panel);
+
+    if (!panel->layout.row) return -2;
+
+    for (row = 0; row < panel->layout.row; row ++)
+    {
+        if ((tempHeight = getRowHeight(panel, row)) >= 0)
+            height += tempHeight;
+    }
+
+    return height;
 }
